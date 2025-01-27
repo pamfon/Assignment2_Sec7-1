0b0e1b2be7497615cd0c7b4329eab5fe22ed2084
pamfon <144912171+pamfon@users.noreply.github.com>
2025-01-27 11:18:44 -0500

    Product class created

---
+++ Assignment2_Sec7-1/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_Sec7_1
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int productID, string productName, double price, int stock)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        public void IncreaseStock()
+        {
+            Stock++;
+        }
+
+        public void DecreaseStock()
+        {
+            if (Stock > 0) { Stock--; }
+        }
+    }
+}
