5fc02946e4d922d32ac37cf9cae6ee61d57b38ea
pamfon <144912171+pamfon@users.noreply.github.com>
2025-02-03 10:33:40 -0500

    Setup the NUnit test project and a sample test

---
+++ ProductTest/ProductTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assignment2_Sec7_1;
+
+namespace ProductTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SampleTest_ShouldPass_WhenValuesAreEqual()
+        {
+            // Arrange
+            int number = 10;
+
+            // Act
+            int numberExpected = 10;
+
+            // Assert
+            Assert.That(number, Is.EqualTo(numberExpected));
+        }
+    }
+}
+
